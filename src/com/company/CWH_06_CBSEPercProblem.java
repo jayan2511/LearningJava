cdd5b8ec1724b65d95800868b5c1b2057e116add
Jayan <jayan2511@gmail.com>
2022-09-11 01:53:52 +0530

    Trying out Commits while learning Java

--- src/com/company/CWH_06_CBSEPercProblem.java
+++ src/com/company/CWH_06_CBSEPercProblem.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 
 public class CWH_06_CBSEPercProblem {
     public static void main(String[] args) {
-
+//      Question- Create a Java Program to accept marks in 5 subjects from user and calculate aggregate percentage, (Marks are out of 100)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks in mathematics: ");
+        float math = sc.nextFloat();
+        System.out.print("Enter marks in english: ");
+        float eng = sc.nextFloat();
+        System.out.print("Enter marks in science: ");
+        float sci = sc.nextFloat();
+        System.out.print("Enter marks in Social Studies: ");
+        float sst = sc.nextFloat();
+        System.out.print("Enter marks in Hindi: ");
+        float hin = sc.nextFloat();
+        float sum = eng+math+sci+sst+hin;
+        float perc = (sum/500)*100;
+        System.out.println("Final Percentage is: ");
+        System.out.println(perc + "%");
     }
 }
