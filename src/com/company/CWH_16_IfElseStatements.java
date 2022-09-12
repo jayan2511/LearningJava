6ff264dee4104781c4c8685f232886fff8c07e29
jayan2511 <jayan2511@gmail.com>
2022-09-12 18:28:28 +0530

    Adding code for if-else-elseif statements tutorial

---
+++ src/com/company/CWH_16_IfElseStatements.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class CWH_16_IfElseStatements {
+    public static void main(String[] args) {
+//        Java has 2 types of conditional statements
+//              1. IF ELSE STATEMENTS
+//              2. SWITCH CASE STATEMENTS
+        int age = 19;
+
+
+//        SYNTAX OF IF ELSE STATEMENT:
+//        if (condition){
+//          statement if condition true
+//        }
+//        else-if(condition){
+//          statement if condition true
+//        }
+//        else{
+//          statement if all conditions false
+//        }
+
+
+
+        if (age>=18){
+            System.out.println("Yes boy you can drive");
+        }
+        else{
+            System.out.println("No boy you cannot drive");
+        }
+
+
+
+    }
+}
