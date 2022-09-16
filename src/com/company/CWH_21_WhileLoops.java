17f25930798f24eb2f347a1f7549321c8516b35e
jayan2511 <jayan2511@gmail.com>
2022-09-16 19:19:55 +0530

    Adding code for while loops in java

---
+++ src/com/company/CWH_21_WhileLoops.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class CWH_21_WhileLoops {
+    public static void main(String[] args) {
+//        There are 3 Kinds of Loops in java
+//        1. While Loops
+//        2. Do-While Loops
+//        3. For Loops
+
+//        Syntax of while loops:
+//        while (condition){
+//          statements
+//        }
+
+
+
+        System.out.println("Using Loops: ");
+        int i=1;
+        while (i<=3){
+            System.out.println(i);
+            i++;
+        }
+
+
+
+        while(true){
+            System.out.println("I am an infinite while loop");
+        }
+
+        
+    }
+}
