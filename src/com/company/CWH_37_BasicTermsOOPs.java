f38dad4ebd54cdeb89c02d11b9d1b5932e99bb05
jayan2511 <jayan2511@gmail.com>
2022-11-03 18:27:45 +0530

    Adding code for basic terms of Object oriented programming

---
+++ src/com/company/CWH_37_BasicTermsOOPs.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class CWH_37_BasicTermsOOPs {
+    public static void main(String[] args) {
+//        Basic terminologies of Object oriented programming
+//        Abstraction: Hiding internal details and only showing essential info
+//        Encapsulation: The act of putting various elements together in a capsule. In java it essentially means that sensitive data can be hidden from the user
+//        Inheritance: Deriving new things from existing things. This implements "do not repeat yourself" (DRY)
+//        Polymorphism: One entity many forms. For eg. you can use a  smartphone as a calculator, phone, etc...
+        
+    }
+}
