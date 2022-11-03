32405ac74c02ac6c7def1b97ed4c446884332b7f
jayan2511 <jayan2511@gmail.com>
2022-11-03 20:21:02 +0530

    Updated Code

--- src/com/company/CWH_37_BasicTermsOOPs.java
+++ src/com/company/CWH_37_BasicTermsOOPs.java
@@ -7,6 +7,6 @@ public class CWH_37_BasicTermsOOPs {
 //        Encapsulation: The act of putting various elements together in a capsule. In java it essentially means that sensitive data can be hidden from the user
 //        Inheritance: Deriving new things from existing things. This implements "do not repeat yourself" (DRY)
 //        Polymorphism: One entity many forms. For eg. you can use a  smartphone as a calculator, phone, etc...
-        
+
     }
 }
