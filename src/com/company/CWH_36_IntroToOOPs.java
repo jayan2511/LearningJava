b3aacba1179e41c681bdb44f7a05f9a40b41fbf2
jayan2511 <jayan2511@gmail.com>
2022-11-03 18:19:09 +0530

    Adding code for Introduction to OOPs

---
+++ src/com/company/CWH_36_IntroToOOPs.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class CWH_36_IntroToOOPs {
+    public static void main(String[] args) {
+//        Object-oriented programming tries to map code instructions to real world making the code short and easier to understand
+//        Object oriented programming is an approach to programming which creates objects to solve problems
+//        DRY refers to "Do not repeat yourself"
+
+//        Class :- Class is a blueprint for crating objects
+//        Class contains info to create a valid object
+//        Memory is only allotted after object instantiation
+
+//        English to OOPs
+//        Noun ---> Class
+//        Adjectives ---> Attributes
+//        Verbs ---> Methods
+
+
+
+
+    }
+}
